8b5344672d1fc02df20231c45e0142ffba853268
dichsa <diegochilet99@gmail.com>
2023-04-11 19:45:46 +0200

    preparando popup

--- app.js
+++ app.js
@@ -13,3 +13,29 @@ $(function() {
 function popUp(URL) {
     window.open(URL, 'Nombre de la ventana', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=300,height=200,left = 390,top = 50');
 }
+
+ function mostrarPopupPr1() {
+    document.querySelector('.popup1').style.display = 'block';
+  }
+  function mostrarPopupPr2() {
+    document.querySelector('.popup2').style.display = 'block';
+  }
+  function mostrarPopupPr3() {
+    document.querySelector('.popup3').style.display = 'block';
+  }
+  function mostrarPopupPr4() {
+    document.querySelector('.popup4').style.display = 'block';
+  }
+  
+  function ocultarPopupPr1() {
+    document.querySelector('.popup1').style.display = 'none';
+  } 
+  function ocultarPopupPr2() {
+    document.querySelector('.popup2').style.display = 'none';
+  } 
+  function ocultarPopupPr3() {
+    document.querySelector('.popup3').style.display = 'none';
+  } 
+  function ocultarPopupPr4() {
+    document.querySelector('.popup4').style.display = 'none';
+  } 
